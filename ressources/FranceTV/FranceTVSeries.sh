89cbd8a8b199951d7377252b2c3c04227bfe24a4
SebTV <109020245+SebTV-prod@users.noreply.github.com>
2025-02-23 08:27:29 +0100

    Update and rename FranceTVSeries.sh to Series.sh

--- ressources/FranceTV/FranceTVSeries.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python3 ressources/ftv/py/FranceTVSeries.py > ressources/ftv/py/FranceTVSeries.m3u8
-
-exit 0
---
+++ ressources/FranceTV/Series.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python3 ressources/ftv/py/FranceTVSeries.py > https://sebtv.fr/channels/Series.m3u8
+
+exit 0
