40a8708c5f26e48b6ddddad3068d602653a7c595
SebTV <109020245+SebTV-prod@users.noreply.github.com>
2025-02-23 08:29:50 +0100

    Update Series.sh

--- ressources/FranceTV/Series.sh
+++ ressources/FranceTV/Series.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 ressources/ftv/py/FranceTVSeries.py > https://sebtv.fr/channels/Series.m3u8
+python3 https://sebtv.fr/channels/Series.py > https://sebtv.fr/channels/Series.m3u8
 
 exit 0
